47950bb136c91c68505305e86240f6775e29ad54
Willian Ferreira <williannfferreira@gmail.com>
2022-04-14 23:10:46 -0300

    chore: add products mock

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
   <main class="my-8">
-    <!-- <default></default> -->
     <div class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
-        <product-card />
+        <product-card v-for="product in products" :key="product.id" />
       </div>
     </div>
   </main>
@@ -15,7 +14,11 @@
 
 <script>
 import ProductCard from '@/components/ProductCard'
+import products from '@/mocks/products.json'
 export default {
   components: { ProductCard },
+  computed: {
+    products: () => products,
+  },
 }
 </script>
